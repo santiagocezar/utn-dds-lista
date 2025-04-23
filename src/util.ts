e8a5deecb81db9fe94b5a562ec5287c63fcefbba
Santiago Cézar <santiagocezar2013@gmail.com>
2025-04-23 14:31:22 -0300

    ups

--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,5 @@
 export function parseCantidad(text: string | undefined) {            
     const cantidad = parseInt(text || "1")
     
-    return isNaN(cantidad) ? 1 : cantidad
+    return isNaN(cantidad) ? 1 : Math.max(1, cantidad)
 }
